c19c242022e115f512f3d8b5cd9cb7a429b8df77
Ashley Stovall <ashley.stovall@cotiviti.com>
2016-05-05 00:35:13 -0400

    sleepy time.

--- src/main/scala/com/rachio/client/Rest.scala
+++ src/main/scala/com/rachio/client/Rest.scala
@@ -3,10 +3,10 @@ package com.rachio.client
 import scalaz._
 import Scalaz._
 import scalaz.concurrent.Task
-import play.api.libs.ws.ning.NingWSClient
+import play.api.libs.ws.ahc.AhcWSClient
 
 private object ClientStuffs {
-  type RClient = NingWSClient
+  type RClient = AhcWSClient
   val version = "1"
 }
 
@@ -27,6 +27,6 @@ trait Device {
 class Driver {
   import ClientStuffs._
   val base = s"https://api.rach.io/${version}/public"
-  implicit val client = NingWSClient()
-  
+  implicit val client = AhcWSClient()
+  client.
 }
